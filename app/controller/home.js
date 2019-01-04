7df9b79e574860157c8daf04a30c5ec651ed2ebc
jiaochunxiao <jcx13627607951@gmail.com>
2019-01-04 20:08:47 +0800

    modify controller

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,34 +3,24 @@ const path = require('path');
 const Controller = require('egg').Controller;
 const Vue = require('vue');
 const fs = require('fs');
-// const renderer = require('vue-server-renderer').createRenderer();
-// const createApp = require(path.resolve(__dirname, '../public/server.js'));
+
 const bundle = fs.readFileSync(path.resolve(__dirname, '../public/server.js'), 'utf-8');
+
 // 注意：此处的renderer为 createBundleRenderer 方法的返回对象
 // bundleRenderer.renderToString([context, callback]): ?Promise<string>
 const renderer = require('vue-server-renderer').createBundleRenderer(bundle, {
+    runInNewContext: false, // 推荐
     template: fs.readFileSync(path.resolve(__dirname, '../public/index.ssr.html'), 'utf-8'),
 });
 
-// const clientRenderer = require('vue-server-renderer').createRenderer({
-//     template: fs.readFileSync(path.join(__dirname, '../public/index.html'), 'utf-8'),
-// });
 class HomeController extends Controller {
     async index() {
         const { ctx } = this;
         const app = new Vue({
             data: {
-                // title: 'Vue ssr',
+                title: 'Vue ssr',
             },
         });
-        const context = {
-            title: 'Vue SSR',
-            meta: `
-                <meta name="description" content="Vue.js 服务端渲染指南">
-            `,
-        };
-        console.log('-----');
-        console.log(context);
         renderer.renderToString(app, (err, html) => {
             if (err) {
                 ctx.status = 500;
